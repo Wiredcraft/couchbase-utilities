70b7339488432bcafeee6a7d352eb4b701dca8db
jiangmengyu0409 <mengyu.jiang@wiredcraft.com>
2021-05-21 13:33:25 +0800

    feat: add params binding for query function

--- index.js
+++ index.js
@@ -145,9 +145,9 @@ function newViewQuery(designDoc, viewName, options) {
  * @param {any} query
  * @returns {Promise<{rows: any[], meta: Couchbase.Bucket.N1qlQueryResponse.Meta}>}
  */
-async function query(bucket, q) {
+async function query(bucket, q, params = {}) {
   return new Promise((resolve, reject) => {
-    bucket.query(q, (err, rows, meta) => {
+    bucket.query(q, params, (err, rows, meta) => {
       if (err) {
         reject(err);
       } else {
@@ -162,8 +162,8 @@ async function queryView(bucket, q) {
   return (await query(bucket, q)).rows;
 }
 
-async function queryN1ql(bucket, q) {
-  return query(bucket, N1qlQuery.fromString(q));
+async function queryN1ql(bucket, q, params = {}) {
+  return query(bucket, N1qlQuery.fromString(q), params);
 }
 
 const paginate = async (
